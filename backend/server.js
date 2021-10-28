f7cba318e61ac4c2c61c55a9ebebb23ddfc8873d
kaijin1234 <93253979+kaijin1234@users.noreply.github.com>
2021-10-28 11:09:52 +0530

    git commit -m

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connect = mongoose.connection;
+connect.once('open', () => {
+	console.log('MongoDB connection successful');
+});
+
+// routing
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
+
+// Defining port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port: ${PORT}`);
+});
